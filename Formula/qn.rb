27f1f171d25987cd7affa859138199be06f557ab
John Mitsch <john@quicknode.com>
2026-06-17 09:38:45 -0400

    qn 0.1.12

--- Formula/qn.rb
+++ Formula/qn.rb
@@ -1,25 +1,25 @@
 class Qn < Formula
   desc "Command-line interface for the Quicknode SDK"
   homepage "https://www.quicknode.com/docs/welcome"
-  version "0.1.11"
+  version "0.1.12"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/quicknode/cli/releases/download/v0.1.11/quicknode-cli-aarch64-apple-darwin.tar.xz"
-      sha256 "8b545bcf6599e47c1379f3c9e8ff6e650e85adada19dd5e61fcd334f531ec952"
+      url "https://github.com/quicknode/cli/releases/download/v0.1.12/quicknode-cli-aarch64-apple-darwin.tar.xz"
+      sha256 "7e7917e6457b705148581472fef50d2f5ba27086667380bc0ed85f79619f9c50"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/quicknode/cli/releases/download/v0.1.11/quicknode-cli-x86_64-apple-darwin.tar.xz"
-      sha256 "6aecda6714bf7027701e2bdad3c16549d05813b7bee699b43acec6b6bac1f36a"
+      url "https://github.com/quicknode/cli/releases/download/v0.1.12/quicknode-cli-x86_64-apple-darwin.tar.xz"
+      sha256 "472799735f40efad8bc79e2a69beb1a3dc14f5bf7768cfc610d3c222794bbf1b"
     end
   end
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/quicknode/cli/releases/download/v0.1.11/quicknode-cli-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "15416c1bb25f13102804b6e374002f55cd7d2d056f2d79cca21ebc11da72101f"
+      url "https://github.com/quicknode/cli/releases/download/v0.1.12/quicknode-cli-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "86fabd9b9c6ed9bd98f8b01415280d7913ec369f446eaf915004290507bf600f"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/quicknode/cli/releases/download/v0.1.11/quicknode-cli-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "1c0ea472c9c323fdeda82d29c53294a5622916c5d48a278df91c7d9a942b16f9"
+      url "https://github.com/quicknode/cli/releases/download/v0.1.12/quicknode-cli-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "812ae898735e9cadea4eefd6b8a46914a58a8d5db320ee2c31c41eb70a7f5307"
     end
   end
   license "MIT"
@@ -66,6 +66,8 @@ class Qn < Formula
     end
 
     install_binary_aliases!
+
+    generate_completions_from_executable(bin/"qn", "completions")
 
     # Homebrew will automatically install these, so we don't need to do that
     doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]

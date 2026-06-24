4432302922495af531b5d3ddf1d30098707cf1f8
John Mitsch <john@quicknode.com>
2026-06-24 11:51:05 -0400

    qn 0.2.1 (#9)

--- Formula/qn.rb
+++ Formula/qn.rb
@@ -1,25 +1,25 @@
 class Qn < Formula
   desc "Command-line interface for the Quicknode SDK"
   homepage "https://www.quicknode.com/docs/welcome"
-  version "0.2.0"
+  version "0.2.1"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/quicknode/cli/releases/download/v0.2.0/quicknode-cli-aarch64-apple-darwin.tar.xz"
-      sha256 "6db4fac4f6a7996e57f0e7aa6b8949058abe9fbe73eb94c9f4c1e1727a244ca9"
+      url "https://github.com/quicknode/cli/releases/download/v0.2.1/quicknode-cli-aarch64-apple-darwin.tar.xz"
+      sha256 "7d6ba8f8d891e423ea2f59d5d55a284f464a00df54973e9ff3a3a381a51ba520"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/quicknode/cli/releases/download/v0.2.0/quicknode-cli-x86_64-apple-darwin.tar.xz"
-      sha256 "1976f75e80908062614a63a8ae2efcb97554810768d1f8cda903dce421c67601"
+      url "https://github.com/quicknode/cli/releases/download/v0.2.1/quicknode-cli-x86_64-apple-darwin.tar.xz"
+      sha256 "741f65021f207e2ea0f188ecca760299441d88764c32610e8b260ea8af841081"
     end
   end
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/quicknode/cli/releases/download/v0.2.0/quicknode-cli-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "1d6b440ead6f88359888fa89ebd2dc470248a549cba72ea4a064fb508abb7775"
+      url "https://github.com/quicknode/cli/releases/download/v0.2.1/quicknode-cli-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "e57e912a60ad560a7f9f84aab0db903990248d2f035c881ef7010b6e3e92045f"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/quicknode/cli/releases/download/v0.2.0/quicknode-cli-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "7b6ad50b427f0e8d3b9eab4b848e7e42b5948fd0b21824ebaac49cc2e18a069d"
+      url "https://github.com/quicknode/cli/releases/download/v0.2.1/quicknode-cli-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "4cc1104c45e609235bd371db9c75daee6c7c5154b1d810b0e8bbaf06303eab0e"
     end
   end
   license "MIT"
